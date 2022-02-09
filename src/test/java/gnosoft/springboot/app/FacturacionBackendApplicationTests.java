eaf812c892e762c66efda67e216bf3c21721e248
Javier Moncada <javierandresmd@ufps.edu.co>
2022-02-09 09:04:45 -0500

    commit base

---
+++ src/test/java/gnosoft/springboot/app/FacturacionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gnosoft.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
